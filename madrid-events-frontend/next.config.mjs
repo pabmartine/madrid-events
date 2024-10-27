652d93b2d8f0e57c4a8269036859971de9a0c870
Pablo Martín Escribano <pablo.martin@io.builders>
2024-10-27 12:32:35 +0100

    fix

--- madrid-events-frontend/next.config.mjs
+++ madrid-events-frontend/next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['www.madrid.es', 'a.tile.openstreetmap.org', 'b.tile.openstreetmap.org', 'c.tile.openstreetmap.org', 'www.tea-tron.com'],
+    unoptimized: true,
   },
   output: 'export',
   experimental: {
